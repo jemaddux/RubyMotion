3ac38eb938b6c4a93a62a353f63655ea1bb28b3f
John Maddux <jemaddux@gmail.com>
2014-03-04 20:45:25 -0500

    working

--- Rakefile
+++ Rakefile
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#encoding: utf-8
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project/template/ios'
 
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,6 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UISCreen.mainScreen.bounds)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @languages_controller = LanguagesController.alloc.initWithNibName(nil, bundle: nil)
     @window.rootViewController = @languages_controller
 
