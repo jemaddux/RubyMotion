f30fe2d441c34821c005a2020f080530fd1d4597
John Maddux <jemaddux@gmail.com>
2014-03-04 21:03:12 -0500

    app delegate

--- lrm-2/app/app_delegate.rb
+++ lrm-2/app/app_delegate.rb
@@ -1,5 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @langauges_controller = LanguagesController.alloc.initWithNibName(nil, bundle: nil)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(@languages_controller)
+
+    @window.makeKeyAndVisible
     true
   end
 end
+
