c468968a284a172a2a38d6d306a08b646c475bbb
John Maddux <jemaddux@gmail.com>
2014-03-13 21:58:29 -0400

    Refactor

--- app/screens/bills_screen.rb
+++ app/screens/bills_screen.rb
@@ -2,21 +2,18 @@ class BillsScreen < ProMotion::GroupedTableScreen
   title "Bills"
 
   def table_data
-    [{cells:
-      [ { title: "Add bill",  action: :add_bill  },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill },
-        { title: "Bill name", action: :view_bill }
-      ]
+    [{cells: cells
     }]
+  end
+
+  def cells
+    @cells ||= begin
+      cellz = [{ title: "Add bill",  action: :add_bill  }]
+      50.times do |x|
+        cellz << { title: "Bill name", action: :view_bill }
+      end
+      cellz
+    end
   end
 
   def add_bill
